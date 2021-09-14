9dabdb1cdd99bf0cbb7713aa9c95e4b139602eb0
Dmitry Dubnovitsky <dmitry@tapcentive.com>
2021-09-15 01:26:06 +0300

    Some fixes.

--- src/component/Header/index.js
+++ src/component/Header/index.js
@@ -7,31 +7,63 @@ import { makeSelectIsAuthorized } from '../../containers/App/selectors';
 import { createStructuredSelector } from 'reselect';
 import { compose } from 'redux';
 import { connect } from 'react-redux';
+import Hidden from '@material-ui/core/Hidden';
+import React from 'react';
+import Box from '@material-ui/core/Box';
 
 const Header = ({ isAuthorized, onRefresh }) => (
-  <div className={styles.wrapper}>
-    <div className={styles.leftContent}>
-      <div className={styles.logo}>
-        <img src={logo} alt="Logo" />
-      </div>
-      <div className={styles.reload} onClick={onRefresh}>
-        <img src={reload} alt="Reload" />
-      </div>
-    </div>
-    <div className={styles.title}>Million Pixel NFT</div>
-    {isAuthorized ? (
-      <div className={styles.wallet}>
-        <div className={styles.walletId}>#123456789</div>
-        <div className={styles.buttonWallet}>
-          <img src={wallet} alt="Wallet" />
+  <>
+    <Hidden smUp>
+      <Box padding={2}>
+        <div className={styles.wrapper}>
+          <div className={styles.leftContent}>
+            <div className={styles.logo}>
+              <img src={logo} alt="Logo" />
+            </div>
+          </div>
+          {isAuthorized ? (
+            <div className={styles.wallet}>
+              <div className={styles.walletId}>#123456789</div>
+              <div className={styles.buttonWallet}>
+                <img src={wallet} alt="Wallet" />
+              </div>
+            </div>
+          ) : (
+            <Link to="/connect">
+              <div className={styles.buttonConnect}>Connect Wallet</div>
+            </Link>
+          )}
         </div>
-      </div>
-    ) : (
-      <Link to="/connect">
-        <div className={styles.buttonConnect}>Connect Wallet</div>
-      </Link>
-    )}
-  </div>
+      </Box>
+    </Hidden>
+    <Hidden xsDown>
+      <Box paddingX={2.4} paddingY={4}>
+        <div className={styles.wrapper}>
+          <div className={styles.leftContent}>
+            <div className={styles.logo}>
+              <img src={logo} alt="Logo" />
+            </div>
+            <div className={styles.reload} onClick={onRefresh}>
+              <img src={reload} alt="Reload" />
+            </div>
+          </div>
+          <div className={styles.title}>Million Pixel NFT</div>
+          {isAuthorized ? (
+            <div className={styles.wallet}>
+              <div className={styles.walletId}>#123456789</div>
+              <div className={styles.buttonWallet}>
+                <img src={wallet} alt="Wallet" />
+              </div>
+            </div>
+          ) : (
+            <Link to="/connect">
+              <div className={styles.buttonConnect}>Connect Wallet</div>
+            </Link>
+          )}
+        </div>
+      </Box>
+    </Hidden>
+  </>
 );
 
 const mapStateToProps = createStructuredSelector({
